a9d0d46bd9d3780098ae9d723b06cd615882a726
gibon228 <kharitonov.oe@gmail.com>
2021-10-17 14:42:59 +0300

    finished

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from products.models import ProductCategory, Product, Basket
+admin.site.register(ProductCategory)
+admin.site.register(Basket)
 
-# Register your models here.
+@admin.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name','image','description', 'short_description',('price','quantity'), 'category')
+    readonly_fields = ('short_description',)
+    ordering = ('name',)
+    search_fields = ('name',)
+
+class BasketInline(admin.TabularInline):
+    model=Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+
--- store/settings.py
+++ store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',g
+    'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/users/login/'
